c1207d67c2918291e21c78d67bb4dcf3d0a9bf5e
bayrakhakan01 <76584714+bayrakhakan01@users.noreply.github.com>
2021-01-22 20:45:16 +0100

    update

--- main.js
+++ main.js
@@ -200,4 +200,25 @@ evil.setControls();
 
 
 
+function loop() {
+  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillRect(0,0,width,height);
+
+  for(let i = 0; i < balls.length; i++) {
+    if(balls[i].exists) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+  }
+
+  evil.draw();
+  evil.checkBounds();
+  evil.collisionDetect();
+
+  requestAnimationFrame(loop);
+}
+
+
+
 loop();
