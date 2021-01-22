29b8031cebb6ee0828511aa6e2d38aeb62b938ab
bayrakhakan01 <76584714+bayrakhakan01@users.noreply.github.com>
2021-01-22 20:44:40 +0100

    change

--- main.js
+++ main.js
@@ -196,24 +196,7 @@ while(balls.length < 25) {
 let evil = new EvilCircle(random(0,width), random(0,height), true);
 evil.setControls();
 
-function loop() {
-  ctx.fillStyle = 'rgba(0,0,0,0.25)';
-  ctx.fillRect(0,0,width,height);
 
-  for(let i = 0; i < balls.length; i++) {
-    if(balls[i].exists) {
-      balls[i].draw();
-      balls[i].update();
-      balls[i].collisionDetect();
-    }
-  }
-
-  evil.draw();
-  evil.checkBounds();
-  evil.collisionDetect();
-
-  requestAnimationFrame(loop);
-}
 
 
 
